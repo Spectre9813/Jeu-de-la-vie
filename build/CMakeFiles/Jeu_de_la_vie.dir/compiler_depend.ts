a09b947a21aa79e30776fa944cb67092e21c5b12
Spectre9813 <arthurchant2006@gmail.com>
2026-03-24 19:11:16 +0100

    Delete build directory

--- build/CMakeFiles/Jeu_de_la_vie.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for Jeu_de_la_vie.
--- build/CMakeFiles/Jeu_de_la_vie.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Jeu_de_la_vie.
-# This may be replaced when dependencies are built.
